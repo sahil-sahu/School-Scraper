2ac6923ec6467c296e83b8c91c3f65505a78f3f2
sahil-sahu <sahilku2003@gmail.com>
2021-01-20 19:28:25 +0530

    school scraped and saved in a file

--- school_scraper/school_scraper/spiders/get_kv.py
+++ school_scraper/school_scraper/spiders/get_kv.py
@@ -3,11 +3,12 @@ import scrapy
 
 class GetKvSpider(scrapy.Spider):
     name = 'get_kv'
-    allowed_domains = ['no3bhubaneswar.kvs.ac.in']
-    start_urls = ['https://no3bhubaneswar.kvs.ac.in/']
+    allowed_domains = ['no1bhubaneswar.kvs.ac.in','no2bhubaneswar.kvs.ac.in','no3bhubaneswar.kvs.ac.in','no4bhubaneswar.kvs.ac.in','no5bhubaneswar.kvs.ac.in','no6bhubaneswar.kvs.ac.in']
+    start_urls = ['https://no1bhubaneswar.kvs.ac.in/','https://no2bhubaneswar.kvs.ac.in/','https://no3bhubaneswar.kvs.ac.in/','https://no4bhubaneswar.kvs.ac.in/','https://no5bhubaneswar.kvs.ac.in/','https://no6bhubaneswar.kvs.ac.in/']
 
     def parse(self, response):
         all_data = response.css("html").extract_first()
-        f = open("../scraped_data/data.html","w")
+        # print(str(response))
+        f = open("../scraped_data/"+str(response).split("https://")[-1].split("/>")[0]+".html","w")
         f.write(all_data)
         f.close()
