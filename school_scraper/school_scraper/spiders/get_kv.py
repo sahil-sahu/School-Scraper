03f022485843739d0b71f6f7a3987374fd52f463
sahil-sahu <sahilku2003@gmail.com>
2021-01-27 08:09:23 +0530

    scraped whole website

--- school_scraper/school_scraper/spiders/get_kv.py
+++ school_scraper/school_scraper/spiders/get_kv.py
@@ -1,14 +1,55 @@
 import scrapy
-
+from scrapy.utils.response import open_in_browser
+import os
 
 class GetKvSpider(scrapy.Spider):
     name = 'get_kv'
-    allowed_domains = ['no1bhubaneswar.kvs.ac.in','no2bhubaneswar.kvs.ac.in','no3bhubaneswar.kvs.ac.in','no4bhubaneswar.kvs.ac.in','no5bhubaneswar.kvs.ac.in','no6bhubaneswar.kvs.ac.in']
-    start_urls = ['https://no1bhubaneswar.kvs.ac.in/','https://no2bhubaneswar.kvs.ac.in/','https://no3bhubaneswar.kvs.ac.in/','https://no4bhubaneswar.kvs.ac.in/','https://no5bhubaneswar.kvs.ac.in/','https://no6bhubaneswar.kvs.ac.in/']
+    # allowed_domains = ['no1bhubaneswar.kvs.ac.in','no2bhubaneswar.kvs.ac.in','no3bhubaneswar.kvs.ac.in','no4bhubaneswar.kvs.ac.in','no5bhubaneswar.kvs.ac.in','no6bhubaneswar.kvs.ac.in']
+    # start_urls = ['https://no1bhubaneswar.kvs.ac.in/','https://no2bhubaneswar.kvs.ac.in/','https://no3bhubaneswar.kvs.ac.in/','https://no4bhubaneswar.kvs.ac.in/','https://no5bhubaneswar.kvs.ac.in/','https://no6bhubaneswar.kvs.ac.in/']
+    start_urls = ['https://no1bhubaneswar.kvs.ac.in/']
+    a_tags = []
 
     def parse(self, response):
         all_data = response.css("html").extract_first()
-        # print(str(response))
-        f = open("scraped_data/"+str(response).split("https://")[-1].split("/>")[0]+".html","w")
+        all_tags = response.css("a")
+        
+        # print(a_tags,sep='\n')
+        BASE_DIR = "../scraped_data/"
+        path = os.path.join(BASE_DIR,str(response).split("https://")[-1].split("/>")[0]) 
+        for i in all_tags:
+            temp = i.xpath("@href").extract_first()
+            # print(i.xpath("@href").extract_first())
+            try:
+                if "http" not in temp and len(temp.split('/')[1::]) > 0 and [''] != temp.split('/')[1::]:
+                    dirs = list(temp.split('/')[1::])
+                    self.a_tags.append(dirs)
+                    self.makedir(path,dirs)
+            except:
+                pass    
+            # i['href'].extract_fist() += "Bala"
+            # print(i['href'].extract_fist())
+        # f = open("scraped_data/"+str(response).split("https://")[-1].split("/>")[0]+".html","w")
+        if not(os.path.isdir(path))  :
+            os.mkdir(path)
+        f = open(path+"/index.html","w")
         f.write(all_data)
         f.close()
+        for i in self.a_tags:
+            yield response.follow("/".join(i), callback=self.temp)
+    
+    def temp(self,response):
+        path = response.url.split("kvs.ac.in/")[1]
+        BASE_DIR = "../scraped_data/"
+        path = os.path.join(BASE_DIR,response.url.split("https://")[1]) 
+        # os.mkdir(path)
+        # print(path,type(path))
+        f = open(path+"index.html","w")
+        f.write(response.css("html").extract_first())
+        f.close() 
+    def makedir(self,path,dirs):
+        if not(os.path.isdir(path+"/"+dirs[0]))  :
+            os.mkdir(path+"/"+dirs[0])
+
+        if not(not(dirs[1::])):
+            dir = list(dirs[1::])    
+            self.makedir(path+"/"+dirs[0],dir)            
